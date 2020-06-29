257e0c07d15ba09f99d4d572e4049f7800805746
hhl <2498393441@qq.com>
2020-06-29 18:15:26 +0800

    新增查询

--- stock/api/urls.py
+++ stock/api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'stock'
 urlpatterns = [
-    path('get_recent_shares', views.get_recent_shares),
+    path('get_shares', views.get_shares),
     path('get_stocks', views.get_stocks),
-    path('get_stock_detail', views.get_stock_detail),
+    path('get_stock', views.get_stock),
 ]
--- stock/api/views.py
+++ stock/api/views.py
@@ -3,15 +3,44 @@ import json
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models import Count, Q
 from django.http import JsonResponse
-from django.core.serializers import serialize
 # Create your views here.
 from stock.models import Stock, Share
 
 
-def get_recent_shares(request):
-    shares = Share.objects.exclude(cash_div_tax=0).order_by('-ann_date')[:100]
+def get_shares(request):
+    """
+    根据查询条件获取的shares
+    :param request:
+    :return:
+    """
+    offset = int(request.GET.get('offset', default=0))
+    page_size = int(request.GET.get('page_size', default=10))
+    page_num = int(request.GET.get('page_num', default=1))
+    prop = request.GET.get('prop', default='ann_date')
+    order = request.GET.get('order', default='descending')
+    ts_code = request.GET.get('ts_code', default=None)
+    start_date = request.GET.get('start_date', default=None)
+    end_date = request.GET.get('end_date', default=None)
+    print(start_date, end_date)
+    # 筛选
+    shares = Share.objects
+    if ts_code:
+        shares = Share.objects.filter(ts_code__ts_code=ts_code)
+    if start_date and end_date:
+        shares = shares.filter(ann_date__lte=end_date, ann_date__gte=start_date)
+    shares = shares.exclude(cash_div_tax=0)
+    total = shares.count()
+    # 排序
+    for i in Share._meta.get_fields():
+        if prop == i.attname:
+            condition = '-' + prop if order == 'descending' else prop
+            shares = shares.order_by(condition)
+    # 划分
+    shares = shares[offset:offset + page_size * page_num]
+
     data = {
-        'shares': json.loads(serialize('json', shares))
+        'total': total,
+        'shares': json.loads(json.dumps(list(shares.values()), cls=DjangoJSONEncoder))
     }
     return JsonResponse(data)
 
@@ -34,7 +63,9 @@ def get_recent_shares(request):
 
 def get_stocks(request):
     """
-    annotate的用法
+    获取stock列表，并在每支股票后标注share次数
+    :param request:
+    :return:
     """
     stocks = Stock.objects.annotate(
         share_times=Count('share', filter=Q(share__div_proc='实施'))
@@ -45,7 +76,12 @@ def get_stocks(request):
     return JsonResponse(data)
 
 
-def get_stock_detail(request):
+def get_stock(request):
+    """
+    获取当前stock的信息，以及其所有share的信息
+    :param request:
+    :return:
+    """
     ts_code = request.GET.get('ts_code')
     stock = Stock.objects.filter(pk=ts_code)
     # 在详情页面也排除每股分红为0的记录
